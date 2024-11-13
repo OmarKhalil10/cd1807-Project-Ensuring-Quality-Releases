9e250832c8e6115217249984b1e6cb1a47d307d2
Omar Khalil <omar.khalil@atos.net>
2024-11-13 07:16:51 +0200

    update azure-pipelines

--- terraform/environments/test/main.tf
+++ terraform/environments/test/main.tf
@@ -1,54 +1,61 @@
 provider "azurerm" {
-  tenant_id       = "${var.tenant_id}"
-  subscription_id = "${var.subscription_id}"
-  client_id       = "${var.client_id}"
-  client_secret   = "${var.client_secret}"
+  tenant_id       = var.tenant_id
+  subscription_id = var.subscription_id
+  client_id       = var.client_id
+  client_secret   = var.client_secret
   features {}
 }
+
 terraform {
   backend "azurerm" {
-    storage_account_name = ""
-    container_name       = ""
-    key                  = ""
-    access_key           = ""
+    storage_account_name = "tfstate103417257"
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
+    access_key           = "a7Neyg9rkSLMhC4harpzupJgwY59kivOARwPFsyi4GwPHDbf5/FzMsBVE2XX7QURGmMwvX6iDnCO+AStaKl3mQ=="
   }
 }
+
+# Use existing resource group
 module "resource_group" {
-  source               = "../../modules/resource_group"
-  resource_group       = "${var.resource_group}"
-  location             = "${var.location}"
+  source         = "../../modules/resource_group"
+  resource_group = var.resource_group
+  location       = var.location
+  create         = false  # Skip creation and just reference it
 }
+
 module "network" {
   source               = "../../modules/network"
-  address_space        = "${var.address_space}"
-  location             = "${var.location}"
-  virtual_network_name = "${var.virtual_network_name}"
-  application_type     = "${var.application_type}"
+  address_space        = var.address_space
+  location             = var.location
+  virtual_network_name = var.virtual_network_name
+  application_type     = var.application_type
   resource_type        = "NET"
-  resource_group       = "${module.resource_group.resource_group_name}"
-  address_prefix_test  = "${var.address_prefix_test}"
+  resource_group       = module.resource_group.resource_group_name  # Use the output name
+  address_prefix_test  = var.address_prefix_test
 }
 
 module "nsg-test" {
   source           = "../../modules/networksecuritygroup"
-  location         = "${var.location}"
-  application_type = "${var.application_type}"
+  location         = var.location
+  application_type = var.application_type
   resource_type    = "NSG"
-  resource_group   = "${module.resource_group.resource_group_name}"
-  subnet_id        = "${module.network.subnet_id_test}"
-  address_prefix_test = "${var.address_prefix_test}"
+  resource_group   = module.resource_group.resource_group_name  # Use the output name
+  subnet_id        = module.network.subnet_id_test
+  address_prefix_test = var.address_prefix_test
 }
+
 module "appservice" {
   source           = "../../modules/appservice"
-  location         = "${var.location}"
-  application_type = "${var.application_type}"
+  location         = var.location
+  application_type = var.application_type
   resource_type    = "AppService"
-  resource_group   = "${module.resource_group.resource_group_name}"
+  resource_group   = module.resource_group.resource_group_name  # Use the output name
 }
+
 module "publicip" {
   source           = "../../modules/publicip"
-  location         = "${var.location}"
-  application_type = "${var.application_type}"
+  location         = var.location
+  application_type = var.application_type
   resource_type    = "publicip"
-  resource_group   = "${module.resource_group.resource_group_name}"
+  resource_group   = module.resource_group.resource_group_name  # Use the output name
 }
--- terraform/environments/test/terraform.tfvars
+++ terraform/environments/test/terraform.tfvars
@@ -1,15 +1,15 @@
 # Azure subscription vars
-subscription_id = ""
-client_id = ""
-client_secret = ""
-tenant_id = ""
+subscription_id = "c79adf2b-9d2a-4e3e-95e8-ef51f2624216"
+client_id = "b222702c-42b9-4918-b6ea-a52dd24c33d2"
+client_secret = "V.u8Q~WTz.rCfTSeK27ThexkH3fBDtyerdSOzaaw"
+tenant_id = "f958e84a-92b8-439f-a62d-4f45996b6d07"
 
 # Resource Group/Location
-location = ""
-resource_group_name = ""
-application_type = ""
+location = "East US 2"
+resource_group = "Azuredevops"
+application_type = "myApplication"
 
 # Network
-virtual_network_name = ""
+virtual_network_name = "myApplication-NET"
 address_space = ["10.5.0.0/16"]
 address_prefix_test = "10.5.1.0/24"
