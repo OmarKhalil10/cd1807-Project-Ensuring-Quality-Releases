186fdd1cf10864d77f25eb56ae78379d255713e3
Omar Khalil <omar.khalil@atos.net>
2024-11-19 06:02:35 +0200

    updated pipeline

--- terraform/modules/vm/vm.tf
+++ terraform/modules/vm/vm.tf
@@ -12,18 +12,18 @@ resource "azurerm_network_interface" "test" {
 }
 
 resource "azurerm_linux_virtual_machine" "test" {
-  name                  = "test-vm"
+  name                  = "${var.resource_type}-${var.application_type}"
   location              = var.location
   resource_group_name   = var.resource_group
   size                  = "Standard_DS2_v2"
-  admin_username        = "azureuser"
+  admin_username        = var.admin_username
 
   network_interface_ids = [
     azurerm_network_interface.test.id
   ]
   
   admin_ssh_key {
-    username   = "azureuser"
+    username   = var.admin_username
     public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCs5/rSO8eBEPKz7YI8VCxsJuXy94gUD/NCDeagK2uOgC1MvCSEGiz82xe+u1FQxGhtWTeR0HaUYo++A5sJ/PoeJarh3QQX6A8JVFbninhvzCWU9jgFt91c+tLJF/pvkL+v+rDXGZB/ZU2uIai5+W+7xCLS2rq4r4MByB93EqpJRSQyB1p6BtIaLnXF3zVV5kBAKaGXh6x7oIiuEUwla0THiL6i270RrDrAFw37Uyz9RFXGS7VhdiThpXt9n7C5ZvanrXmPWjPy0MzN/FdMRftQFFWmzWDaZEf+ofqramTSQk5/p38hJ6r9LepBpSrJtZjkyjORjYotrcqkdPlN+c+L3Aiwl/awrlacyOI2r3tQz23n71yUbe5/3yda+gMu+xZQhOsNulWdd+AmOaCHHyID+oCLKD5FYBM5y67j0n0s6zwCm9lunmbyYrIWGJbc9H1K6QNtGqNOm9cz4DeA5+kQ6FWjlCwbnMXFeaS9AVZFWrSwJoQti2imy8HIvXjBpPM= WW930+a923717@DESKTOP-6PQM6SL"
   }
   
