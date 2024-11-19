ec00c3f2b61f57c26e8dcf93151d0a69d5b4183c
Omar Khalil <omar.khalil@atos.net>
2024-11-19 03:41:07 +0200

    updated pipeline

--- terraform/modules/vm/vm.tf
+++ terraform/modules/vm/vm.tf
@@ -12,28 +12,30 @@ resource "azurerm_network_interface" "test" {
 }
 
 resource "azurerm_linux_virtual_machine" "test" {
-  name                  = "${var.resource_type}-${var.application_type}"
+  name                  = "test-vm"
   location              = var.location
   resource_group_name   = var.resource_group
-  size                  = "Basic_A1"
-  admin_username        = var.admin_username
+  size                  = "Standard_DS2_v2"
+  admin_username        = "azureuser"
 
   network_interface_ids = [
     azurerm_network_interface.test.id
   ]
   
   admin_ssh_key {
-    username   = var.admin_username
+    username   = "azureuser"
     public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCs5/rSO8eBEPKz7YI8VCxsJuXy94gUD/NCDeagK2uOgC1MvCSEGiz82xe+u1FQxGhtWTeR0HaUYo++A5sJ/PoeJarh3QQX6A8JVFbninhvzCWU9jgFt91c+tLJF/pvkL+v+rDXGZB/ZU2uIai5+W+7xCLS2rq4r4MByB93EqpJRSQyB1p6BtIaLnXF3zVV5kBAKaGXh6x7oIiuEUwla0THiL6i270RrDrAFw37Uyz9RFXGS7VhdiThpXt9n7C5ZvanrXmPWjPy0MzN/FdMRftQFFWmzWDaZEf+ofqramTSQk5/p38hJ6r9LepBpSrJtZjkyjORjYotrcqkdPlN+c+L3Aiwl/awrlacyOI2r3tQz23n71yUbe5/3yda+gMu+xZQhOsNulWdd+AmOaCHHyID+oCLKD5FYBM5y67j0n0s6zwCm9lunmbyYrIWGJbc9H1K6QNtGqNOm9cz4DeA5+kQ6FWjlCwbnMXFeaS9AVZFWrSwJoQti2imy8HIvXjBpPM= WW930+a923717@DESKTOP-6PQM6SL"
   }
+  
   os_disk {
-    caching           = "ReadWrite"
+    caching              = "ReadWrite"
     storage_account_type = "Standard_LRS"
   }
+  
   source_image_reference {
     publisher = "Canonical"
     offer     = "UbuntuServer"
-    sku       = "16.04-LTS"
+    sku       = "18.04-LTS"
     version   = "latest"
   }
 }
