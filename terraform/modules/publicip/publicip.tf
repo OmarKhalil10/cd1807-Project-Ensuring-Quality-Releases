f761010f7499d53d457f3c694ed75d21dea92429
Omar Khalil <omar.khalil@atos.net>
2024-11-19 03:30:23 +0200

    updated pipeline

--- terraform/modules/publicip/publicip.tf
+++ terraform/modules/publicip/publicip.tf
@@ -1,7 +1,11 @@
 resource "azurerm_public_ip" "test" {
   name                = "${var.application_type}-${var.resource_type}-pubip"
-  location            = "${var.location}"
-  resource_group_name = "${var.resource_group}"
-  allocation_method   = "Dynamic"
+  location            = var.location
+  resource_group_name = var.resource_group
+  allocation_method   = "Static"  # Must be Static for Standard SKU
+  sku                  = "Standard"  # Ensure Standard SKU is used
 
+  tags = {
+    environment = "testing"
+  }
 }
