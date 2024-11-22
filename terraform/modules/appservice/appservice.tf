04b8c0a3334ae7636d5a1d3c2498c8ed6cf3df73
Omar Khalil <omar.khalil@atos.net>
2024-11-22 22:09:01 +0200

    update app service

--- terraform/modules/appservice/appservice.tf
+++ terraform/modules/appservice/appservice.tf
@@ -1,26 +1,25 @@
 resource "azurerm_service_plan" "test" {
   name                = "${var.application_type}-${var.resource_type}"
-  location            = var.location
-  resource_group_name = var.resource_group
-  os_type             = "Windows" # Updated to Windows for ASP.NET runtime
+  location            = "${var.location}"
+  resource_group_name = "${var.resource_group}"
+  os_type             = "Windows"
   sku_name            = "F1"
 }
 
-resource "azurerm_windows_web_app" "test" {
+resource "azurerm_linux_web_app" "test" {
   name                = "${var.application_type}-${var.resource_type}"
-  location            = var.location
-  resource_group_name = var.resource_group
+  location            = "${var.location}"
+  resource_group_name = "${var.resource_group}"
   service_plan_id     = azurerm_service_plan.test.id
 
+  app_settings = {
+    "WEBSITE_RUN_FROM_PACKAGE" = 0
+  }
   site_config {
     always_on = false
-    ftps_state = "Disabled"
-
-    # Set the .NET Framework runtime
-    windows_fx_version = "ASPNet|V4.8" # Runtime for ASP.NET Framework 4.8
+    application_stack {
+      dotnet_version = "4.0"
+    }
   }
 
-  app_settings = {
-    "WEBSITE_RUN_FROM_PACKAGE" = "1" # Set app setting separately
-  }
 }
